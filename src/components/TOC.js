5803abd5dbbee4c85deddba326d635303ac1f9a3
tazz009 <tazz0009@gmail.com>
2020-05-06 11:39:10 +0900

    Create Project

---
+++ src/components/TOC.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class TOC extends Component {
+    shouldComponentUpdate(newProps, newState) {
+        if (this.props.data === newProps.data) {
+            return false;
+        }
+        return true;
+    }
+    render() {
+        var lists = [];
+        var data = this.props.data;
+        var i = 0;
+        while (i < data.length) {
+            lists.push(
+                <li key={data[i].id}>
+                    <a 
+                        href={"/content/" + data[i].id}
+                        data-id={data[i].id}
+                        onClick={function(id, e){
+                            e.preventDefault();
+                            this.props.onChangePage(id);
+                        }.bind(this, data[i].id)}
+                    >{data[i].title}</a>
+                </li>
+            );
+            i++;
+        }
+        return (
+        <nav>
+            <ul>
+                {lists}
+            </ul>
+        </nav>
+        );
+    }
+}
+
+export default TOC;
